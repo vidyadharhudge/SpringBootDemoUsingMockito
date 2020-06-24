1be4907cb730634ac9ffa8379ff34def3ad3f43f
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-06-24 04:11:29 -0700

    Added:-Welcome Message

---
+++ src/main/java/com/example/springbootmockito/SpringbootmockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmockito/SpringbootmockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
